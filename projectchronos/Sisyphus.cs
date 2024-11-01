fe1eaf273e09243e8cc403f98945bb3083d6e24b
muhammed-hab <72678073+muhammed-hab@users.noreply.github.com>
2024-11-01 15:04:01 -0500

    Create the sisyphus enemy

--- projectchronos/Sisyphus.cs
+++ projectchronos/Sisyphus.cs
@@ -2,7 +2,7 @@
 
 public partial class Sisyphus: BasicEnemy {
     public override void _Ready() {
-        stats = new StatBlock(1000, 50, true, true, npcAI.DefaultAI, 1000);
+        stats = new StatBlock(1000, 50, true, true, npcAI.SisyphusAI, 1000);
         base._Ready();
     }
 }
