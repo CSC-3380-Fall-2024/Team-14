4ff2f613129139ab80b2384b0ec56f648c6c3482
muhammed-hab <72678073+muhammed-hab@users.noreply.github.com>
2024-11-09 16:30:03 -0600

    Made sisyphus roll the rock when the player is within a certain distance of sisyphus.

--- projectchronos/Sisyphus.cs
+++ projectchronos/Sisyphus.cs
@@ -1,10 +1,29 @@
-﻿namespace ProjectChronos;
+namespace ProjectChronos;
 
-public partial class Sisyphus: BasicEnemy {
-    public override void _Ready()
-    {
-        MaxLife = 1000;
-        CurrentLife = 1000;
-        base._Ready();
-    }
+public partial class Sisyphus: BasicEnemy, BasicEnemy.EnemyAI {
+	public override void _Ready()
+	{
+		MaxLife = 1000;
+		CurrentLife = 1000;
+		ai = this;
+		base._Ready();
+	}
+
+	public float DistanceToRoll = 400f;
+	public float SecondsToRoll = 1f;
+
+	private float lastRolled;
+	
+	public void ExecuteAI(float delta)
+	{
+		if (DistanceToPlayer() < DistanceToRoll)
+		{
+			lastRolled += delta;
+			if (lastRolled > SecondsToRoll)
+			{
+				lastRolled -= SecondsToRoll;
+				GetParent().AddChild(Rollingrock.CreateRock(Position, PlayerPosition()));
+			}
+		}
+	}
 }
