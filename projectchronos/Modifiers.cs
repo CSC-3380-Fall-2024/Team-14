80dc3698d8b5ce9ede552a737d740e2922068864
jhelaire <jhelai5@lsu.edu>
2024-11-09 08:19:55 -0600

    fixed settings and upgrade screen not following player

--- projectchronos/Modifiers.cs
+++ projectchronos/Modifiers.cs
@@ -1,36 +1,44 @@
 using Godot;
 using System;
+using System.ComponentModel.Design;
 
 public partial class Modifiers : Control
 {
+	
 
 	public override void _Ready()
 	{
 		base._Ready();
 		Visible = false;
-		ProcessMode = ProcessModeEnum.WhenPaused;
+		ProcessMode = ProcessModeEnum.Always;
+		GetNode<Button>("/root/Main/CanvasLayer/Modifiers/HBoxContainer/Mod2").Disabled = true;
+		GetNode<Button>("/root/Main/CanvasLayer/Modifiers/HBoxContainer/Mod3").Disabled = true;
 	}
 
 	public override void _Process(double delta)
 	{
-		if (Visible == true)
-		{
-			GetTree().Paused = true;
-		}
+		
 	}
+
 
 	private void OnModOnePressed()
 	{
 		 Visible = false;
+		 GetTree().Paused = false;
+		 GetNode<Button>("/root/Main/CanvasLayer/Modifiers/HBoxContainer/Mod2").Disabled = false;
+
 	}
 	
 	private void OnModTwoPressed()
 	{
 		 Visible = false;
+		 GetTree().Paused = false;
+		 GetNode<Button>("/root/Main/CanvasLayer/Modifiers/HBoxContainer/Mod3").Disabled = false;
 	}
 	
 	private void OnModThreePressed()
 	{
 		 Visible = false;
+		 GetTree().Paused = false;
 	}
 }
