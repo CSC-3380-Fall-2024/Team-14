dc888a12ca8d4437b6399360ee5b47c9ca58c71b
jhelaire <jhelai5@lsu.edu>
2024-11-13 11:46:24 -0600

    testing death screen

---
+++ projectchronos/GameOver.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class GameOver : CanvasLayer
+{
+    private void OnRestartPressed()
+    {
+        GetTree().ChangeSceneToFile("res://main.tscn");
+    }
+
+    private void OnQuitToTitlePressed()
+    {
+        GetTree().ChangeSceneToFile("title_screen.tscn");
+    }
+}
--- projectchronos/Main.cs
+++ projectchronos/Main.cs
@@ -5,28 +5,11 @@ using ProjectChronos;
 
 public partial class Main : Node {
 
-	private TextureRect exitScreen;
-
 	private Configuration config = new() { MaxLives = 3, FireDamagePerSecond = 2 };
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
-		//initailize exit screen but hide it so player doesnt't just see DIED lol
-		exitScreen = new TextureRect();
-		exitScreen.Texture = GD.Load<Texture2D>("res://exit.png");
-		//makes hidden
-		exitScreen.Visible = false;
-
-		//set size and position
-		exitScreen.AnchorTop = 0;
-		exitScreen.AnchorBottom = 1;
-		exitScreen.AnchorLeft=0;
-		exitScreen.AnchorRight=1;
-
-		exitScreen.StretchMode = TextureRect.StretchModeEnum.Scale;
-
-		AddChild(exitScreen);
-
+		
 		//Call start on player
 		Player player = GetNode<Player>("World/Player");
 		player.Start(new Godot.Vector2 (0,0));
@@ -40,11 +23,4 @@ public partial class Main : Node {
 	public Configuration getConfig() {
 		return this.config;
 	}
-
-//shows the exit screen and basically just unhides it after death
-	public void ShowExit()
-	{
-		exitScreen.Visible = true;
-	}
-
 }
