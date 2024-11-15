9bec576fa159449a35912ace349b04f84cc80a1d
Zetavblitz <147882829+Zetavblitz@users.noreply.github.com>
2024-11-15 12:59:02 -0600

    Setting Screen Complete
    The setting screen is now complete with proper functionality. However, it does not work mid-game due to the ongoing migration to the tilemap.

--- projectchronos/SettingsScreen.cs
+++ projectchronos/SettingsScreen.cs
@@ -2,57 +2,47 @@ using Godot;
 using System;
 using System.Runtime.CompilerServices;
 
-// silly little enum for silly little screen ratios
-public enum AspectRatio {
-	SixteenNine,
-	FourThree
-}
-
 public partial class SettingsScreen : Control {
-	// Local variables store option selections
-	private bool isHardMode;
-	private AspectRatio ratio;
-
 	// we will need references to some specific child nodes (these display text)
 	private Label normalLabel;
 	private Label hardLabel;
 	private Label labelSixteenNine;
 	private Label labelFourThree;
 
-	public override void _Ready() {
-		isHardMode = false; // normal mode by default
-		ratio = AspectRatio.SixteenNine; // who even uses 4:3?
+	private TitleScreen titleScreen;
 
+	public override void _Ready() {
 		// acquire the specific nodes for the option text labels
 		normalLabel = GetNode<Label>("SettingsOptions/GAMEPLAY SETTINGS/NormalLabel");
 		hardLabel = GetNode<Label>("SettingsOptions/GAMEPLAY SETTINGS/HardLabel");
 		labelSixteenNine = GetNode<Label>("SettingsOptions/GENERAL SETTINGS/LabelSixteenNine");
 		labelFourThree = GetNode<Label>("SettingsOptions/GENERAL SETTINGS/LabelFourThree");
+
+		// acquire the title screen node
+		titleScreen = GetParent<TitleScreen>();
 	}
 
-	public bool IsHardMode() {
-		return isHardMode;
-	}
+    public override void _PhysicsProcess(double delta) {
+		normalLabel.Visible = !titleScreen.GetDifficulty();
+		hardLabel.Visible = titleScreen.GetDifficulty();
+		labelFourThree.Visible = titleScreen.GetScreenRatio() == AspectRatio.FourThree;
+		labelSixteenNine.Visible = titleScreen.GetScreenRatio() == AspectRatio.SixteenNine;
+    }
 
-	// from difficulty selection connected in Godot
-	// difficulty button ON is hard mode
-	private void OnDifficultyButtonToggled(bool toggled) {
-		isHardMode = toggled;
-		normalLabel.Visible = !toggled;
-		hardLabel.Visible = toggled;
+    // from difficulty selection connected in Godot
+    // difficulty button ON is hard mode
+    private void OnDifficultyButtonToggled(bool toggled) {
+		titleScreen.SetDifficulty(toggled);
 	}
 
 	// from aspect ratio selection, connected in Godot
 	// button toggled ON is 4:3, default (off) 16:9
 	private void OnAspectRatioButtonToggled(bool toggled) {
 		if (toggled) {
-			ratio = AspectRatio.FourThree;
+			titleScreen.SetAspectRatio(AspectRatio.FourThree);
 		} else {
-			ratio = AspectRatio.SixteenNine;
+			titleScreen.SetAspectRatio(AspectRatio.SixteenNine);
 		}
-
-		labelSixteenNine.Visible = !toggled;
-		labelFourThree.Visible = toggled;
 	}
 
 	// Signal handler method for continue button
@@ -62,6 +52,6 @@ public partial class SettingsScreen : Control {
 
 	// Signal handler method for quit to title button
 	private void OnQuitToTitleButtonPressed() {
-		GetTree().ChangeSceneToFile("res://title_screen.tscn"); // Go to title screen scene
+		Visible = false; // Go to title screen scene
 	}
 }
--- projectchronos/TitleScreen.cs
+++ projectchronos/TitleScreen.cs
@@ -1,33 +1,58 @@
 using Godot;
 using System;
 
-public partial class TitleScreen : Control
-{
+// silly little enum for silly little screen ratios
+public enum AspectRatio {
+	SixteenNine,
+	FourThree
+}
+
+public partial class TitleScreen : Control {
+	private bool isHardMode;
+	private AspectRatio screenRatio;
+
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready() 
-	{
-		ProcessMode = ProcessModeEnum.Always; //makes it so that title screen works when paused;
+	public override void _Ready()  {
+		ProcessMode = ProcessModeEnum.Always; // makes it so that title screen works when paused;
+		isHardMode = false; // normal mode by default
+		screenRatio = AspectRatio.SixteenNine; // who even uses 4:3?
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)  {
 	}
 
-	//Defining the signal handler method for the start button to be connected via the Godot editor
+	// Defining the signal handler method for the start button to be connected via the Godot editor
 	private void OnStartButtonPressed() {
-		GetTree().ChangeSceneToFile("res://main.tscn");
+		GetTree().ChangeSceneToFile("res://tartarus_level.tscn");
 	}
 
-	//Defining the signal handler method for the quit button to be connected via the Godot editor
+	// Defining the signal handler method for the quit button to be connected via the Godot editor
 
 	private void OnQuitButtonPressed() {
 		GetTree().Quit();
 	}
 
-	//Defining the signal handler method for the settings button to be connected via the Godot editor
+	// Defining the signal handler method for the settings button to be connected via the Godot editor
 	private void OnSettingsButtonPressed() {
 		SettingsScreen settings = GetNode<SettingsScreen>("SettingsScreen");
 		settings.Visible = true;
 		
+	}
+
+	public bool GetDifficulty() {
+		return isHardMode;
+	}
+
+	public void SetDifficulty(bool isHardMode) {
+		this.isHardMode = isHardMode;
+	}
+
+	public AspectRatio GetScreenRatio() {
+		return screenRatio;
+	}
+
+	public void SetAspectRatio(AspectRatio screenRatio) {
+		this.screenRatio = screenRatio;
 	}
 }
