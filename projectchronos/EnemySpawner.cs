a3654d7ec048a219e7692dac1336c87632c7051f
jhelaire <jhelai5@lsu.edu>
2024-11-25 16:22:34 -0600

    minor upgrade fix

--- projectchronos/EnemySpawner.cs
+++ projectchronos/EnemySpawner.cs
@@ -3,12 +3,13 @@ using System;
 
 public partial class EnemySpawner : Node2D
 {
+	public Timer SpawnTimer;
 	[Export]
 	public PackedScene EnemyScene {get; set;}
 	public override void _Ready()
 	{
 		base._Ready();
-		var SpawnTimer = new Timer();
+		SpawnTimer = new Timer();
 		SpawnTimer.WaitTime = 2;
 		SpawnTimer.OneShot = false;
 		SpawnTimer.Autostart = true;
