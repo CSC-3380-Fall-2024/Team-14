dfc10ec57956c5dc1633c388241dc451c9aff98b
Zetavblitz <147882829+Zetavblitz@users.noreply.github.com>
2024-12-03 16:09:48 -0600

    Removed Gray Jump Bar
    Removed the gray bar that displays above the player that is visible when the player can double jump.

--- projectchronos/enemies/BasicEnemy.cs
+++ projectchronos/enemies/BasicEnemy.cs
@@ -26,7 +26,7 @@ public partial class BasicEnemy : CharacterBody2D {
 	private PlayerAttack playerAttack;
 
 	public override void _Ready() {
-		playerAttack = player.GetChild<PlayerAttack>(7);
+		playerAttack = player.GetChild<PlayerAttack>(6);
 	}
 
 	public override void _PhysicsProcess(double delta) {
